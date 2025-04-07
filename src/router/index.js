687e683c82a609231606ce8bc0071d1926c6c647
David Kester <davidkester@Davids-MacBook-Air-2.local>
2025-04-07 20:38:47 +0200

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import Banner from '../components/Banner.vue';
+import SolarDeckView from '../views/SolarDeckView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +10,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: Banner,
+    },
+    {
+      path: '/solardeck',
+      name: 'solardeck',
+      component: SolarDeckView,
     }
   ]
   
---
+++ src/views/SolarDeckView.vue
@@ -0,0 +1,105 @@
+<script setup>
+</script>
+
+<template>  
+
+  <div class="main-container">
+
+    <div class="container">
+
+      <div class="row">
+        <h1>Revolutionize Yout Fleet with SolarDeck</h1>
+        <div>Utilize Solar Power for Cost-Efficient and Sustainable Short-Sea Shipping</div>
+        <small>SolarDeck on a Multi Purpose Cargo Vessel</small>
+      </div>
+
+      <hr>
+
+      <div class="row">
+        <h2>Key Benefits</h2>
+
+        <div class="col-4">
+          <div>Reduced Dayrate</div>
+        </div>
+
+        <div class="col-4">
+          <div>Comply with Regulations</div>
+        </div>
+
+        <div class="col-4">
+          <div>Execellet RIO</div>
+        </div>      
+      </div>
+
+      <div class="row">
+       
+        <div class="col-6">
+          <div>Introducing SolarDeck- a proven solution, now tailored for short-sea shipping.</div>
+          <div>SolarDeck provides a sustainable, cost-effective alternative to tradicinal power sources, enhancing your operational efficiency while reducing environment impact and generating an attractive financial return.</div>
+        </div>
+
+        <div class="col-6">
+          <img href="#">
+        </div>
+    
+      </div>
+
+      <hr>
+
+      <div class="row">
+        <h2>Effect on Regulations</h2>  
+      </div>
+
+      <hr>
+
+      <div class="row">
+        <h2>SolarDeck Facts</h2>  
+
+        <div class="col-5">
+
+          <ul>
+            <li>ROI 2-5 years</li>
+            <li>IRR of 25%</li>
+            <li>Installed in 8 hours</li>
+            <li>Annual energy production of 106 MWH</li>
+            <li>Yearly fuel reduction of 30 mt</li>
+            <li>Yearly C02 reduction of 96 mt</li>
+            <li>Easily implemented on both newbuild and existing vessels</li>
+            <li>Simple to operate</li>
+            <li>Liva data insights</li>
+          </ul>
+
+        </div>
+
+        <div class="col-7">
+          <img href="#">
+        </div>
+
+        <small>Based on MS Vertom Anette</small>
+
+      </div>
+
+    </div>
+  </div>
+
+
+</template>
+
+<style scoped>
+
+  ul {
+    list-style: none;
+  }
+
+  ul li:before {
+    content: '✓';
+  }
+
+  .main-container {
+    min-height: 100%;
+    background: rgb(4, 17, 29);
+    color: #ffffff;
+  }
+
+</style>
+
